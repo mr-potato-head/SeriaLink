67e6df430e54e2c79fefa3ef2192e2aeacb6da9a
Guilhem GUYONNET <guilhem.guyonnet@addupsolutions.com>
2020-02-20 13:12:34 +0100

    Update copyright year.

--- src/comportmanager.cc
+++ src/comportmanager.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 Guilhem GUYONNET
+ * Copyright (C) 2020 Guilhem GUYONNET
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
